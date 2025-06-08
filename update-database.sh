5894781b4d28c224e6c079a567fbb472d406923a
Jonathan Wessler <jonathan.m.wessler@gmail.com>
2025-06-08 19:43:09 -0400

    shell script logs path, title, composer, genre, and voicing in test database as shell arguments

--- update-database.sh
+++ update-database.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-title=$1
-composer=$2
-genre=$3
-voicing=$4
+yaml_file=docs/_data/testdb.yml
+title="$1"
+composer="$2"
+genre="$3"
+voicing="$4"
+path="$5"
 
-echo $1$2$3$4 >> docs/_data/testdb.yml
+echo "" >> $yaml_file
+echo "-" >> $yaml_file
+echo "  path: $5" >> $yaml_file
+echo "  title: $1" >> $yaml_file
+echo "  composer: $2" >> $yaml_file
+echo "  genre: $3" >> $yaml_file
+echo "  voicing: $4" >> $yaml_file
