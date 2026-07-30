36bd0150c5859921d21de29e2e2198bbe33fecb6
Jonathan Wessler <jonathan.m.wessler@gmail.com>
2026-07-30 15:54:46 -0400

    echo yq output and exit codes

--- find-empty-keys.sh
+++ find-empty-keys.sh
@@ -1,5 +1,13 @@
-!#/usr/bin/env bash
+#!/usr/bin/env bash
 
 ## this script returns any record in the database that has a key a missing value
 
-yq '.[] | select(.[] == null or .[] == "")' docs/_data/database.yml
+yqOutput=$(yq '.[] | select(.[] == null or .[] == "")' docs/_data/database.yml)
+
+if [[ -z "$yqOutput" ]]; then
+	echo 'No empty keys!'
+	exit 0
+else
+	echo "$yqOutput"
+	exit 1
+fi
